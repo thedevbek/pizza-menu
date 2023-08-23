0c0d798ce90a1fbac4ccccee140bd42e2dbd490f
Bek <thedevbek@proton.me>
2023-08-22 21:07:36 -0500

    last fix

--- src/index.js
+++ src/index.js
@@ -86,17 +86,17 @@ const Menu = () => {
 	);
 };
 
-function Pizza(props) {
-	if (props.pizzaObj.soldOut) return null;
+function Pizza({pizzaObj}) {
+	if (pizzaObj.soldOut) return null;
 
 	return (
 		<li className='pizza'>
-			<img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+			<img src={pizzaObj.photoName} alt={pizzaObj.name} />
 			<div>
-				<h3>{props.pizzaObj.name}</h3>
-				<p>{props.pizzaObj.ingredients}</p>
-				<span>{props.pizzaObj.price}</span>
-				<p>{props.pizzaObj.soldOut}</p>
+				<h3>{pizzaObj.name}</h3>
+				<p>{pizzaObj.ingredients}</p>
+				<span>{pizzaObj.price}</span>
+				<p>{pizzaObj.soldOut}</p>
 			</div>
 		</li>
 	);
@@ -115,7 +115,7 @@ const Footer = () => {
 	return (
 		<footer className='footer'>
 			{isOpen ? (
-				<Order closeHour={closeHour} />
+				<Order closeHour={closeHour}  openHour={openHour}/>
 			) : (
 				<p>
 					We're happy to welcome you between {openHour}:00 and {closeHour}:00.
@@ -125,11 +125,11 @@ const Footer = () => {
 	);
 };
 
-function Order(props) {
+function Order({ openHour, closeHour }) {
 	return (
 		<div className='order'>
 			<p>
-				We're open until {props.closeHour}:00. Come visit us or order online
+				We're open from {openHour}:00 to {closeHour}:00. Come visit us or order online
 			</p>
 			<button className='btn'>Order</button>
 		</div>
